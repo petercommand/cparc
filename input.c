95c8e86fcdb4276cade946bf8bc43e04248db8f1
Petercommand <petercommand@gmail.com>
2015-10-29 00:36:22 +0800

    prototyping

---
+++ input.c
@@ -0,0 +1,27 @@
+#include "input.h"
+
+
+input* input_new(char* input) {
+  input* i = (input *) calloc(sizeof(input));
+  i->line = 1;
+  i->pos = 1;
+  i->cursor = 1;
+  i->input = input;
+  return i;
+}
+
+void input_delete(input* i) {
+  free(i);
+}
+
+input* input_next(input * i) {
+  if(i->input[i->cursor] == '\n') {
+    i->line++;
+    i->pos = 1;
+    i->cursor++;
+  }
+  else {
+    i->pos++;
+    i->cursor++;
+  }
+}
---
+++ input.h
@@ -0,0 +1,11 @@
+#ifndef INPUT_H
+#define INPUT_H
+
+typedef struct {
+  int line;
+  int pos;
+  int cursor;
+  char* input;
+} input;
+
+#endif INPUT_H
