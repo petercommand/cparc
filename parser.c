569754ad31da8c1933a63fc9d069b1d0d81431ef
petercommand <petercommand@gmail.com>
2015-11-19 12:01:46 +0800

    fix typo

--- parser.c
+++ parser.c
@@ -415,7 +415,7 @@ parser* parser_chain(list* parsers, size_t num, input_t input) {
     head = head->next;
   }
   sc->allow_empty = allow_empty;
-  dynamic_parser_closure* dpc = dynamic_parser_closure_new_p(ctxe, parser_chain_dp);
+  dynamic_parser_closure* dpc = dynamic_parser_closure_new_p(ctxes, parser_chain_dp);
   return parser_new(sc, dpc);
 }
 
